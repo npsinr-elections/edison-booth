4a183dc7cda427dbc5e2bfce5b495c3738d98fe9
Muktaka Joshipura <muktaka2000@gmail.com>
2018-05-28 03:45:08 +0530

    feat: Added home.html

--- src/server/routes/mainRouter.ts
+++ src/server/routes/mainRouter.ts
@@ -1,5 +1,7 @@
 import express = require("express");
+import ip = require("ip");
 
+import { config } from "../../config";
 import { election } from "../../dummy/dummyElection";
 
 export const router = express.Router();
@@ -8,12 +10,17 @@ router.get("/", (_REQ, res) => {
   res.redirect("/home");
 });
 
-router.get("/home", (_REQ, res) => {
-  res.render("welcome.html");
+router.get("/home", (req, res) => {
+  res.render("home.html", {
+    appName: config.appName,
+    currentURL: req.url,
+    pageTitle: "Home",
+    lanIP: ip.address()
+  });
 });
 
 router.get("/vote", (_REQ, res) => {
-  res.render("vote.html", { election: election });
+  res.render("vote/vote.html", { election: election });
 });
 
 router.post("/vote", (req, res) => {
